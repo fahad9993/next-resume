38478ca596bb249a15fc839f8e1080f71a6f8a5b
Fahad <fahad9993@gmail.com>
2025-05-12 10:53:46 +0600

    Add timeline for industrial training

--- component/Courses.tsx
+++ component/Courses.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLocationDot, FaRegCalendarDays } from "react-icons/fa6";
+import { FaLocationDot, FaRegCalendarDays, FaClock } from "react-icons/fa6";
 
 export default function Courses() {
   return (
@@ -7,9 +7,13 @@ export default function Courses() {
       <h2 className="section-title">Training</h2>
       <h4 className="section-h2 mt-2">Industrial Training</h4>
       <p className="mt-1">110 MW & 115 MW Barge-Mounted Power Plant</p>
-      <div className="flex items-baseline space-x-4 text-sm">
-        <div className="flex items-center space-x-2 mt-1">
+      <div className="flex items-baseline space-x-4 text-sm mt-1">
+        <div className="flex items-center space-x-2">
           <FaRegCalendarDays />
+          <span>08/2016</span>
+        </div>
+        <div className="flex items-center space-x-2">
+          <FaClock />
           <span>21 days</span>
         </div>
         <div className="flex items-center space-x-2">
